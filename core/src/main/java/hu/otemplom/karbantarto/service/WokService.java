11d88fe7c9bd7911970f55f47a0ad862b30e96a9
misi <mpekar55@gmail.com>
2020-09-14 15:12:11 +0200

    typo fix in workservice

--- core/src/main/java/hu/otemplom/karbantarto/service/WokService.java
+++ core/src/main/java/hu/otemplom/karbantarto/service/WokService.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 public interface WokService {
     int AddWork(Work work);
     boolean ModifyWork(Work work) throws WorkDoesNotExistsException;
-    boolean DeleteWorkById(int wokrId)throws WorkDoesNotExistsException;
+    boolean DeleteWorkById(int workId)throws WorkDoesNotExistsException;
     Work getWorkById(int workId) throws WorkDoesNotExistsException;
     Collection<Work> getNewWorks();
     Collection<Work> getWorksByUserId(int userId);
