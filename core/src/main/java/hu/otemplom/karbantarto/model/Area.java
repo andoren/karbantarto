f78d2cd4f29e4089a4d81f46bb2be1c402e4d6f0
misi <mpekar55@gmail.com>
2020-09-14 13:59:14 +0200

    area propertyk és mezők legenerálva

--- core/src/main/java/hu/otemplom/karbantarto/model/Area.java
+++ core/src/main/java/hu/otemplom/karbantarto/model/Area.java
@@ -1,4 +1,31 @@
 package hu.otemplom.karbantarto.model;
 
 public class Area {
+    private int id;
+    private String name;
+    private User boss;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getBoss() {
+        return boss;
+    }
+
+    public void setBoss(User boss) {
+        this.boss = boss;
+    }
 }
