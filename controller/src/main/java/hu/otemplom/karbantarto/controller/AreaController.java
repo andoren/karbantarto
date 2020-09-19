da2731db9ecfa14bccec667afde85e848ed8387e
andoren <mpekar55@gmail.com>
2020-09-19 21:31:32 +0200

    area controllerbol megirva a getAreaById, deleteArea,modifyarea.

--- controller/src/main/java/hu/otemplom/karbantarto/controller/AreaController.java
+++ controller/src/main/java/hu/otemplom/karbantarto/controller/AreaController.java
@@ -5,11 +5,13 @@ import hu.otemplom.karbantarto.model.Area;
 import hu.otemplom.karbantarto.model.Exceptions.Area.InvalidIdException;
 import hu.otemplom.karbantarto.service.AreaService;
 import hu.otemplom.karbantarto.service.Exceptions.AreaService.AreaAlreadyExistsException;
+import hu.otemplom.karbantarto.service.Exceptions.AreaService.AreaDoesNotExistsException;
 import hu.otemplom.karbantarto.service.Exceptions.AreaService.InvalidAreaException;
 import hu.otemplom.karbantarto.service.impl.AreaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Collection;
 import java.util.List;
 @RequestMapping("api/v1/area")
@@ -29,4 +31,18 @@ public class AreaController {
     public void addArea(@RequestBody Area area) throws InvalidIdException, InvalidAreaException, AreaAlreadyExistsException {
         areaService.addArea(area);
     }
+    @PutMapping
+    public void modifyArea(@RequestBody Area area) throws InvalidAreaException, AreaDoesNotExistsException {
+        areaService.modifyArea(area);
+    }
+
+    @DeleteMapping(path="{id}")
+    public void deleteArea(@PathVariable("id") int id) throws AreaDoesNotExistsException {
+        areaService.deleteAreaById(id);
+    }
+    @GetMapping(path="{id}")
+    public Area getAreaById(@PathVariable("id")int id) throws AreaDoesNotExistsException {
+        return areaService.getAreaById(id);
+    }
+
 }
