c745096372566fb3162ffcb34ac407672db18541
misi <mpekar55@gmail.com>
2020-09-21 14:08:48 +0200

    fakeWorkDataAccess Megirva

--- dao/src/main/java/hu/otemplom/karbantarto/dao/WorkDao.java
+++ dao/src/main/java/hu/otemplom/karbantarto/dao/WorkDao.java
@@ -1,12 +1,17 @@
 package hu.otemplom.karbantarto.dao;
 
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidDoneDateException;
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidIdException;
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidProceedDateException;
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidWorkerException;
 import hu.otemplom.karbantarto.model.Work;
+import hu.otemplom.karbantarto.service.Exceptions.UserService.UserDoesNotExistsException;
 import hu.otemplom.karbantarto.service.Exceptions.WorkService.WorkDoesNotExistsException;
 
 import java.util.Collection;
 
 public interface WorkDao {
-    int addWork(Work work);
+    int addWork(Work work) throws InvalidIdException;
     boolean modifyWork(Work work) throws WorkDoesNotExistsException;
     boolean deleteWorkById(int workId)throws WorkDoesNotExistsException;
     Work getWorkById(int workId) throws WorkDoesNotExistsException;
@@ -15,7 +20,7 @@ public interface WorkDao {
     Collection<Work> getStartedWorks();
     Collection<Work> getNeedToCheckWorks();
     Collection<Work> getThisMonthDoneWorks();
-    boolean setWorkStarted(int workId,int userId) throws WorkDoesNotExistsException;
-    boolean setWorkProcceed(int workId) throws WorkDoesNotExistsException;
-    boolean setWorkDone(int workId) throws WorkDoesNotExistsException;
+    boolean setWorkStarted(int workId,int userId) throws WorkDoesNotExistsException, InvalidWorkerException, UserDoesNotExistsException;
+    boolean setWorkProcceed(int workId) throws WorkDoesNotExistsException, InvalidProceedDateException;
+    boolean setWorkDone(int workId) throws WorkDoesNotExistsException, InvalidDoneDateException;
 }
--- dao/src/main/java/hu/otemplom/karbantarto/dao/fakeWorkDataAccessDao.java
+++ dao/src/main/java/hu/otemplom/karbantarto/dao/fakeWorkDataAccessDao.java
@@ -1,69 +1,94 @@
 package hu.otemplom.karbantarto.dao;
 
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidDoneDateException;
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidIdException;
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidProceedDateException;
+import hu.otemplom.karbantarto.model.Exceptions.Work.InvalidWorkerException;
 import hu.otemplom.karbantarto.model.Work;
+import hu.otemplom.karbantarto.service.Exceptions.UserService.UserDoesNotExistsException;
 import hu.otemplom.karbantarto.service.Exceptions.WorkService.WorkDoesNotExistsException;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Repository("fakeWorkDao")
 public class fakeWorkDataAccessDao implements WorkDao {
+    private static List<Work> dummyDB = new ArrayList<>();
     @Override
-    public int addWork(Work work) {
-        return 0;
+    public int addWork(Work work) throws InvalidIdException {
+        work.setId(dummyDB.size() +1);
+        dummyDB.add(work);
+        return work.getId();
     }
 
     @Override
     public boolean modifyWork(Work work) throws WorkDoesNotExistsException {
-        return false;
+        Work modifyThis = getWorkById(work.getId());
+        int index = dummyDB.indexOf(modifyThis);
+        dummyDB.set(index,work);
+        return true;
     }
 
     @Override
     public boolean deleteWorkById(int workId) throws WorkDoesNotExistsException {
-        return false;
+        Work deleteThis = getWorkById(workId);
+        dummyDB.remove(deleteThis);
+        return true;
     }
 
     @Override
     public Work getWorkById(int workId) throws WorkDoesNotExistsException {
-        return null;
+        return dummyDB.stream().filter(w->w.getId() == workId).findFirst().orElseThrow(()-> new WorkDoesNotExistsException(""));
     }
 
     @Override
     public Collection<Work> getNewWorks() {
-        return null;
+        return dummyDB.stream().filter(w->w.getWorker() == null).collect(Collectors.toList());
     }
 
     @Override
     public Collection<Work> getWorksByUserId(int userId) {
-        return null;
+        return dummyDB.stream().filter(w->w.getOwner().getId() == userId).collect(Collectors.toList());
     }
 
     @Override
-    public Collection<Work> getStartedWorks() {
-        return null;
+    public Collection<Work> getStartedWorks()
+    {
+        return dummyDB.stream().filter(w->w.getWorker() != null && w.getProceedDate() == null).collect(Collectors.toList());
     }
 
     @Override
     public Collection<Work> getNeedToCheckWorks() {
-        return null;
+
+        return dummyDB.stream().filter(w->w.getProceedDate() != null && !w.getIsDone()).collect(Collectors.toList());
     }
 
     @Override
-    public Collection<Work> getThisMonthDoneWorks() {
-        return null;
+    public Collection<Work> getThisMonthDoneWorks()
+    {
+        return dummyDB.stream().filter(w->w.getIsDone()).collect(Collectors.toList());
     }
 
     @Override
-    public boolean setWorkStarted(int workId, int userId) throws WorkDoesNotExistsException {
-        return false;
+    public boolean setWorkStarted(int workId, int userId) throws WorkDoesNotExistsException, UserDoesNotExistsException, InvalidWorkerException {
+        Work work = getWorkById(workId);
+        fakeUserDataAccessDao userDataAccessDao = new fakeUserDataAccessDao();
+        work.setWorker(userDataAccessDao.getUserByUserId(userId));
+        return true;
     }
 
     @Override
-    public boolean setWorkProcceed(int workId) throws WorkDoesNotExistsException {
-        return false;
+    public boolean setWorkProcceed(int workId) throws WorkDoesNotExistsException, InvalidProceedDateException {
+        Work work = getWorkById(workId);
+        work.setProceedDate(new Date());
+        return true;
     }
 
     @Override
-    public boolean setWorkDone(int workId) throws WorkDoesNotExistsException {
-        return false;
+    public boolean setWorkDone(int workId) throws WorkDoesNotExistsException, InvalidDoneDateException {
+        Work work = getWorkById(workId);
+        work.setDoneDate(new Date());
+        return true;
     }
 }
